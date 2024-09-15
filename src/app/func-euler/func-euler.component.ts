e4a3f4ff69595edc73e51e822e598c0910c7120d
Vaynbaum <mr.vaynbaum@mail.ru>
2024-09-15 14:33:18 +0300

    1 file modified

--- src/app/func-euler/func-euler.component.ts
+++ src/app/func-euler/func-euler.component.ts
@@ -27,37 +27,49 @@ export class FuncEulerComponent implements OnInit {
     arr = this.fermaService.ferma_hidden(n);
 
     if (arr[0] + arr[1] == 1 || arr[0] - arr[1] == 1) {
-      if (
-        this.dict.find((obj) => {
-          return obj.num == arr[0] + arr[1];
-        })
-      )
-        this.dict.find((obj) => {
-          if (obj.num == arr[0] + arr[1]) obj.cnt++;
-        });
-      else {
-        let num = arr[0] + arr[1];
-        let cnt = 1;
-        this.dict.push({ num, cnt });
-      }
+      let num = arr[0] + arr[1];
+      let obj = this.dict.find((obj) => obj.num == num);
+      if (obj) obj.cnt++;
+      else this.dict.push({ num: num, cnt: 1 });
 
-      if (
-        this.dict.find((obj) => {
-          return obj.num == arr[0] - arr[1];
-        })
-      )
-        this.dict.find((obj) => {
-          if (obj.num == arr[0] - arr[1]) obj.cnt++;
-        });
-      else {
-        let num = arr[0] - arr[1];
-        let cnt = 1;
-        this.dict.push({ num, cnt });
-      }
+      num = arr[0] - arr[1];
+      obj = this.dict.find((obj) => obj.num == num);
+      if (obj) obj.cnt++;
+      else this.dict.push({ num: num, cnt: 1 });
     } else {
       this.allFact(arr[0] + arr[1]);
       this.allFact(arr[0] - arr[1]);
     }
+  }
+
+  displayResult() {
+    let mult = 1;
+    let answer = `Ответ: &#966;(${this.n}) = &#966;(`;
+    let answer_tail = `) = `;
+
+    this.dict = this.dict.sort((a, b) => a.num - b.num);
+    this.dict.forEach((obj) => {
+      if (obj.num != 1) {
+        if (obj.cnt == 1) answer += `${obj.num} * `;
+        else answer += `${obj.num}<sup>${obj.cnt}</sup> * `;
+
+        answer_tail += `(${obj.num}${
+          obj.cnt == 1 ? '' : `<sup>${obj.cnt}</sup>`
+        }-${obj.cnt - 1 > 0 ? obj.num : 1}${
+          obj.cnt - 1 > 1 ? `<sup>${obj.cnt - 1}</sup>` : ''
+        })`;
+
+        mult *= Math.pow(obj.num, obj.cnt) - Math.pow(obj.num, obj.cnt - 1);
+      }
+    });
+    answer = answer.slice(0, -3);
+    answer += answer_tail;
+
+    let element: HTMLElement = document.getElementById(
+      'euresult'
+    ) as HTMLElement;
+    answer += ` = ${mult}`;
+    element.innerHTML = answer;
   }
 
   calc() {
@@ -66,27 +78,13 @@ export class FuncEulerComponent implements OnInit {
       let num = this.n;
 
       while (num % 2 == 0) {
-        if (this.dict.find((obj) => obj.num == 2))
-          this.dict.find((obj) => {
-            if (obj.num == 2) obj.cnt++;
-          });
+        let obj = this.dict.find((obj) => obj.num == 2);
+        if (obj) obj.cnt++;
         else this.dict.push({ num: 2, cnt: 1 });
         num /= 2;
       }
-      console.log(this.dict);
       this.allFact(num);
-
-      let mult = 1;
-      this.dict.forEach((obj) => {
-        if (obj.num != 1)
-          mult *= Math.pow(obj.num, obj.cnt) - Math.pow(obj.num, obj.cnt - 1);
-      });
-
-      let element: HTMLElement = document.getElementById(
-        'euresult'
-      ) as HTMLElement;
-      let answer = `Ответ: &#966;(${this.n}) = ${mult}`;
-      element.innerHTML = answer;
+      this.displayResult();
     }
   }
 }
