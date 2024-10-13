a0765782ba2cc7569dfee3c78a8a30a5650050ff
Vaynbaum <mr.vaynbaum@mail.ru>
2024-10-13 09:52:18 +0300

    2 files modified, 1 added

--- src/app/primitive-root/primitive-root.component.ts
+++ src/app/primitive-root/primitive-root.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FactorizationService } from '../shared/services/factorisation.service';
 
 @Component({
   selector: 'app-primitive-root',
   templateUrl: './primitive-root.component.html',
-  styleUrls: ['./primitive-root.component.css']
+  styleUrls: ['./primitive-root.component.css'],
 })
 export class PrimitiveRootComponent implements OnInit {
+  m: number | undefined;
 
-  constructor() { }
+  constructor(private factorizationService: FactorizationService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  public onChange(m: number) {
+    this.m = m;
   }
 
+  calc() {}
 }
