6d57148b3cbe4f06bb816b607c9a5a0f39757fa4
Vaynbaum <mr.vaynbaum@mail.ru>
2024-10-19 20:37:04 +0300

    2 files modified, 1 added

--- src/app/indicative-comparison-2/indicative-comparison-2.component.ts
+++ src/app/indicative-comparison-2/indicative-comparison-2.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-indicative-comparison-2',
   templateUrl: './indicative-comparison-2.component.html',
-  styleUrls: ['./indicative-comparison-2.component.css']
+  styleUrls: ['./indicative-comparison-2.component.css'],
 })
 export class IndicativeComparison2Component implements OnInit {
+  a: number | undefined;
+  b: number | undefined;
+  m: number | undefined;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  public onChangeA(a: number) {
+    this.a = a;
   }
-
+  public onChangeB(b: number) {
+    this.b = b;
+  }
+  public onChangeM(m: number) {
+    this.m = m;
+  }
+  ngOnInit() {}
+  calc() {
+    
+  }
 }
